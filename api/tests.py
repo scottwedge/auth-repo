9901e8ffa41dc6050cc3a5a8b4c109eb6f1848f9
swedge <scott.wedge@gmail.com>
2020-03-13 23:09:38 -0400

    Fixes #1

--- api/tests.py
+++ api/tests.py
@@ -6,7 +6,7 @@ class UsersManagersTest(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(email="normal@user.com", password="password")
-        self.asertEqual(user.email, 'normal@user.com')
+        self.assertEqual(user.email, 'normal@user.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
